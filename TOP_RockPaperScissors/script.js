7e5c685e6ba1f5083f5a15b9dc9b981c9840dc99
Akif Zahin <zahinakif25@gmail.com>
2024-06-24 08:03:12 +0600

    initial commit

---
+++ TOP_RockPaperScissors/script.js
@@ -0,0 +1,12 @@
+function humanInput()
+{
+   let hInput = prompt("Choose! Rock, Paper or Scissors?");
+   let lowerhInput = hInput.toLowerCase();
+   return lowerhInput;
+
+}
+function computerInput()
+{
+    let cInput = [rock,paper,scissors];
+    
+}
